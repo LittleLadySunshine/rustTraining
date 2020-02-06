321639e95b8fc9d67a165eae72fa5d034ac6fd91
Denise McCoy <denisemccoy@DeniseFSTLY2.local>
2020-02-06 14:33:20 -0700

    Initial Rust commit

---
+++ hello_world/target/debug/deps/hello_world-caf9b7ba6021fddc.d
@@ -0,0 +1,5 @@
+/Users/denisemccoy/Desktop/rustProjects/hello_world/target/debug/deps/hello_world-caf9b7ba6021fddc: src/main.rs
+
+/Users/denisemccoy/Desktop/rustProjects/hello_world/target/debug/deps/hello_world-caf9b7ba6021fddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/denisemccoy/Desktop/rustProjects/hello_world/target/debug/hello_world: /Users/denisemccoy/Desktop/rustProjects/hello_world/src/main.rs
