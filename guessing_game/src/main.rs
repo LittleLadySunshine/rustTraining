0b5ad30e6512b97bac1091f44ef15f31dbfe43c5
Denise McCoy <denisemccoy@DeniseFSTLY2.local>
2020-02-06 15:54:09 -0700

    finished Guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering; //cmp() method used for comparing
 use rand::Rng;
 
 fn main() {
@@ -10,13 +11,33 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("failed to read line");
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("failed to read line");
 
-    println!("You guessed: {}", guess);
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            }; //The parse() method on strings parses a string into some kind of number. Since it can parse a variety of numbers, we need to give Rust a hint as to the exact type of number we want.
+
+
+        //Rust allows us to ‘shadow’ the previous guess with a new one. This is often used in this exact situation, where guess starts as a String, but we want to convert it to an u32. Shadowing lets us re-use the guess name, rather than forcing us to come up with two unique names like guess_str and guess, or something else.
+
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less    => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal   => {
+                    println!("You win!");
+                    break;
+            }
+        }
+    }
 }
