fa549c79bd6f5a2f0ab22fe74a11b18a4595b40e
Denise McCoy <denise.l.mccoy@gmail.com>
2020-02-10 10:56:40 -0700

    added documentation around concurrency

--- diningPhilosophers/dining_philosophers/src/main.rs
+++ diningPhilosophers/dining_philosophers/src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+//use brings names into scope. We’re going to start using the thread module from the standard library, and so we need to use it.
 
 struct Philosopher {
     name: String,
@@ -13,6 +14,8 @@ impl Philosopher {
 
     fn eat(&self) {
         println!("{} is done eating.", self.name);
+        thread::sleep_ms(1000);
+        println!("{} is done eating.", self.name);
     }
 }
 
@@ -25,7 +28,15 @@ fn main() {
         Philosopher::new("Michel Foucault"),
     ];
 
-    for p in &philosophers {
-        p.eat();
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+          thread::spawn(move || {
+              p.eat();
+              //Here’s where the concurrency happens. The thread::spawn function takes a closure as an argument and executes that closure in a new thread. This closure needs an extra annotation, move, to indicate that the closure is going to take ownership of the values it’s capturing. Primarily, the p variable of the map function.
+          })
+      }).collect();
+
+      for h in handles {
+          h.join().unwrap();
+          //At the end of main(), we loop through the handles and call join() on them, which blocks execution until the thread has completed execution. This ensures that the threads complete their work before the program exits.
     }
 }
