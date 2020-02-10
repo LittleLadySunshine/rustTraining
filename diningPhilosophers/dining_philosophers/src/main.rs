0c8209b3483c4d4aa77a420103a130e79ca13e52
Denise McCoy <denise.l.mccoy@gmail.com>
2020-02-10 11:05:26 -0700

    finished concurrency and added notes

--- diningPhilosophers/dining_philosophers/src/main.rs
+++ diningPhilosophers/dining_philosophers/src/main.rs
@@ -1,39 +1,60 @@
 use std::thread;
 //use brings names into scope. We’re going to start using the thread module from the standard library, and so we need to use it.
+use std::sync::{Mutex, Arc};
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str,left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} is done eating.", self.name);
         thread::sleep_ms(1000);
         println!("{} is done eating.", self.name);
     }
 }
 
-fn main() {
-    let philosophers = vec![
-        Philosopher::new("Baruch Spinoza"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Marx"),
-        Philosopher::new("Friedrich Nietzsche"),
-        Philosopher::new("Michel Foucault"),
-    ];
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
-    let handles: Vec<_> = philosophers.into_iter().map(|p| {
-          thread::spawn(move || {
-              p.eat();
-              //Here’s where the concurrency happens. The thread::spawn function takes a closure as an argument and executes that closure in a new thread. This closure needs an extra annotation, move, to indicate that the closure is going to take ownership of the values it’s capturing. Primarily, the p variable of the map function.
-          })
-      }).collect();
+fn main()
+    let table = Arc::new(Table { forks: vec![
+      Mutex::new(()),
+      Mutex::new(()),
+      Mutex::new(()),
+      Mutex::new(()),
+      Mutex::new(()),
+  ]});
+
+  let philosophers = vec![
+      Philosopher::new("Baruch Spinoza", 0, 1),
+      Philosopher::new("Gilles Deleuze", 1, 2),
+      Philosopher::new("Karl Marx", 2, 3),
+      Philosopher::new("Friedrich Nietzsche", 3, 4),
+      Philosopher::new("Michel Foucault", 0, 4),
+  ];
+
+  let handles: Vec<_> = philosophers.into_iter().map(|p| {
+      let table = table.clone();
+
+      thread::spawn(move || {
+          p.eat(&table);
+      })
+  }).collect();
 
       for h in handles {
           h.join().unwrap();
